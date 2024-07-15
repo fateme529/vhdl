d061604934c3232cdbc799851aea1ebebd247aa0
fateme529 <121169758+fateme529@users.noreply.github.com>
2024-07-15 15:56:11 +0330

    rom

---
+++ Behaviroral/ROM/dynamic_rom_Behavioral/SRC/dynamic_rom.vhdl
@@ -0,0 +1,28 @@
+LIBRARY IEEE;
+USE IEEE.STD_LOGIC_1164.ALL;
+USE IEEE.STD_LOGIC_UNSIGNED.ALL;
+
+ENTITY dynamic_rom IS
+    GENERIC (
+        adr_size : INTEGER := 3;
+        data_size : INTEGER := 16);
+    PORT (
+        adr : IN STD_LOGIC_VECTOR(adr_size - 1 DOWNTO 0);
+        data : OUT STD_LOGIC_VECTOR(data_size - 1 DOWNTO 0)
+
+    );
+END dynamic_rom;
+
+ARCHITECTURE behavioral OF dynamic_rom IS
+
+    TYPE dynamic_rom_type IS ARRAY ((2 ** adr_size - 1) TO 7) OF STD_LOGIC_VECTOR(data_size - 1 DOWNTO 0);
+    CONSTANT my_dynamic_rom : dynamic_rom_type :=
+    (
+    X"0123", X"4567", X"89AB", X"CDEF",
+    X"AAAA", X"BBBB", X"CCCC", X"DDDD"
+    );
+
+BEGIN
+    data <= my_dynamic_rom(CONV_INTEGER(adr));
+END ARCHITECTURE;
+--fatemeh_damavandi
---
+++ Behaviroral/ROM/dynamic_rom_Behavioral/SRC/tb_dynamic_rom.vhdl
@@ -0,0 +1,31 @@
+LIBRARY ieee;
+USE ieee.std_logic_1164.ALL;
+
+ENTITY test_dynamic_rom IS
+    GENERIC (
+        adr_size : INTEGER := 3;
+        data_size : INTEGER := 16);
+END test_dynamic_rom;
+
+ARCHITECTURE test OF test_dynamic_rom IS
+    COMPONENT dynamic_rom IS
+        GENERIC (
+            adr_size : INTEGER := 3;
+            data_size : INTEGER := 16);
+
+        PORT (
+            adr : IN STD_LOGIC_VECTOR(adr_size - 1 DOWNTO 0);
+            data : OUT STD_LOGIC_VECTOR(data_size - 1 DOWNTO 0)
+
+        );
+    END COMPONENT;
+    FOR ALL : dynamic_rom USE ENTITY work.dynamic_rom(behavioral);
+    SIGNAL adr : STD_LOGIC_VECTOR(adr_size - 1 DOWNTO 0);
+    SIGNAL data : STD_LOGIC_VECTOR(data_size - 1 DOWNTO 0);
+BEGIN
+
+    uut : dynamic_rom GENERIC MAP(adr_size, data_size) PORT MAP(adr, data);
+    adr <= "000", "001" AFTER 40 ns, "010" AFTER 50 ns, "011" AFTER 60 ns, "100" AFTER 70 ns, "101" AFTER 80 ns, "111" AFTER 90 ns;
+
+END ARCHITECTURE;
+--fatemeh_damavandi

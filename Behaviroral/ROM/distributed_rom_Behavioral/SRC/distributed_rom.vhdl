23e785693dda1c2f76d95940d88846d9f818b941
fateme529 <121169758+fateme529@users.noreply.github.com>
2024-07-15 15:52:38 +0330

    Rom

---
+++ Behaviroral/ROM/distributed_rom_Behavioral/SRC/distributed_rom.vhdl
@@ -0,0 +1,26 @@
+LIBRARY IEEE;
+USE IEEE.STD_LOGIC_1164.ALL;
+USE IEEE.STD_LOGIC_UNSIGNED.ALL;
+
+ENTITY distributed_rom IS
+    PORT (
+        adr : IN STD_LOGIC_VECTOR(2 DOWNTO 0);
+        data : OUT STD_LOGIC_VECTOR(15 DOWNTO 0)
+
+    );
+END distributed_rom;
+
+ARCHITECTURE behavioral OF distributed_rom IS
+
+    TYPE distributed_rom_type IS ARRAY (0 TO 7) OF STD_LOGIC_VECTOR(15 DOWNTO 0);
+    CONSTANT my_distributed_rom : distributed_rom_type :=
+    (
+    X"0123", X"4567", X"89AB", X"CDEF",
+    X"AAAA", X"BBBB", X"CCCC", X"DDDD"
+    );
+
+BEGIN
+
+    data <= my_distributed_rom(CONV_INTEGER(adr));
+END ARCHITECTURE;
+--fatemeh_damavandi
---
+++ Behaviroral/ROM/distributed_rom_Behavioral/SRC/tb_distributed_rom.vhdl
@@ -0,0 +1,26 @@
+LIBRARY ieee;
+USE ieee.std_logic_1164.ALL;
+
+ENTITY test_distributed_rom IS
+END test_distributed_rom;
+
+ARCHITECTURE test OF test_distributed_rom IS
+    COMPONENT distributed_rom IS
+        PORT (
+            adr : IN STD_LOGIC_VECTOR(2 DOWNTO 0);
+            data : OUT STD_LOGIC_VECTOR(15 DOWNTO 0)
+
+        );
+    END COMPONENT;
+    FOR ALL : distributed_rom USE ENTITY work.distributed_rom(behavioral);
+
+    SIGNAL adr : STD_LOGIC_VECTOR(2 DOWNTO 0);
+    SIGNAL data : STD_LOGIC_VECTOR(15 DOWNTO 0);
+BEGIN
+
+    uut : distributed_rom PORT MAP(adr, data);
+
+    adr <= "000", "001" AFTER 40 ns, "010" AFTER 50 ns, "011" AFTER 60 ns, "100" AFTER 70 ns, "101" AFTER 80 ns, "111" AFTER 90 ns;
+
+END ARCHITECTURE;
+--fatemeh_damavandi
